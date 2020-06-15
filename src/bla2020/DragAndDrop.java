6c0d722b8b795f5f8df490534720e304949cf32b
Mahfuj Tuhin <Mahfuj Tuhin@MAHFUJ-1B5SPLO>
2020-06-14 23:23:43 -0400

    bullet point added

---
+++ src/bla2020/DragAndDrop.java
@@ -0,0 +1,39 @@
+package bla2020;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class DragAndDrop {
+
+	@Test
+	public void oprnBrowser() throws Exception {
+System.setProperty("webdriver.chrome.driver", "C:\\Users\\Mahfuj Tuhin\\eclipse-workspace\\BatchMarch2020\\jars\\chromedriver_win32\\chromedriver.exe");
+		
+		
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://jqueryui.com/droppable/");
+		driver.manage().window().maximize();
+		System.out.println(driver.getTitle());
+		
+		java.util.List<WebElement> frame = driver.findElements(By.tagName("iframe"));
+		
+		System.out.println(frame.size());
+		
+		driver.switchTo().frame(0);
+		System.out.println(driver.findElement(By.xpath("//*[@id=\"draggable\"]")).getText());
+		System.out.println(driver.findElement(By.xpath("//*[@id=\"droppable\"]")).getText());
+		
+		WebElement drag = driver.findElement(By.xpath("//*[@id=\"draggable\"]"));
+	    WebElement drop = driver.findElement(By.xpath("//*[@id=\"droppable\"]"));
+	    
+	    Actions act=new Actions(driver);
+	    Thread.sleep(3000);
+	    act.dragAndDrop(drag, drop).build().perform();
+
+	}
+
+}
